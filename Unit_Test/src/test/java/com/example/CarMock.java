a02b167ef26981e75469d8050d2ca58d7614c2af
Dony Thomas <dony.thomas7@gmail.com>
2014-12-19 16:22:34 -0500

    6th commit

--- Unit_Test/src/test/java/com/example/CarMock.java
+++ Unit_Test/src/test/java/com/example/CarMock.java
@@ -3,6 +3,8 @@ package com.example;
 import static org.mockito.Mockito.*;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CarMock {
@@ -12,7 +14,15 @@ public class CarMock {
 	@Test
 	public void testCarInstance() {
 		assertTrue(myCar instanceof Car);
-		
+		assertEquals(0.0, myCar.getEngineTemperature(), 0.0);
+		when(myCar.getEngineTemperature()).thenReturn(2.0);
+		assertEquals(2.0, myCar.getEngineTemperature(), 0.0);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testCarException() {
+		when(myCar.getEngineTemperature()).thenThrow(new NullPointerException());
+		myCar.getEngineTemperature();
 	}
 
 }
